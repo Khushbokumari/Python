f82405263fc95ae86b6c0d8dee8ef694dabf378c
Khushboo kumari <91077129+Sanvi99999@users.noreply.github.com>
2022-09-16 16:57:21 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,39 @@
+import math
+print ("hello")
+print(math.gcd(4,7))
+a=2
+b=3
+s=a+b
+
+if a>=b:
+    s=0
+else:
+   s=a-b
+print(s)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+typeA=type(a)
+print(typeA)
+b=4
+typeb=type(b) 
+print(b-9)      
+c="hello, welcome to the python"
+d="khushi"
+print(c)
+f="66"
+f=float(f)
+print(c[1:5])
+print(c.strip())
+print(len(c))
+print(f)
+var =c.lower()
+var =c.upper()
+var =c.replace("el","mn")
+var =c.replace(",",'\n')
+print(c+d)
+temp="hello{0} khushi{1} ".format(c,d)
+temp=f
+print(temp)
+
